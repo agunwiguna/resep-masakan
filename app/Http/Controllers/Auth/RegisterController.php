53136fd269f63fe768d3b1f4444a2b553e429063
agunwiguna <wgnagun@gmail.com>
2019-01-22 13:48:10 +0700

    ubah role

--- app/Http/Controllers/Auth/RegisterController.php
+++ app/Http/Controllers/Auth/RegisterController.php
@@ -69,7 +69,7 @@ class RegisterController extends Controller
             'username' => $data['username'],
             'email' => $data['email'],
             'password' => Hash::make($data['password']),
-            'role' => 'admin',
+            'role' => 'user',
         ]);
     }
 }
